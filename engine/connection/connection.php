304f9adc165b647046441025a3f81a4a56a6d59a
sovesttut <56912470+sovesttut@users.noreply.github.com>
2021-01-06 16:19:38 +0500

    include engine

---
+++ engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0502054_root';
+$password = 'root';
+$database = 'f0502054_user';
+?>
---
+++ engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Алсу Рахмангулова ЛР№5
+        </title>
+	</head>
+</html>
