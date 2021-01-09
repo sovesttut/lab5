f2176d85fd560bd162866889573ed0dff19141af
sovesttut <56912470+sovesttut@users.noreply.github.com>
2021-01-09 15:57:12 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,19 +6,10 @@
 			if(isset($_GET['id'])&&isset($_GET['query'])){
                 $id = htmlentities(mysqli_real_escape_string($link, $_GET['id']));
                 $index = htmlentities(mysqli_real_escape_string($link, $_GET['query']));
-                if($index=="Banks"){
-                    $index="Banks";
-                    $query = "DELETE FROM $database.$index WHERE id_bank='$id'";
+                if($index=="session_info"){
+                    $index="session";
                 }
-                if($index=="deposit_programs_info"){
-                    $index="deposit_programs";
-                    $query = "DELETE FROM $database.$index WHERE id_deposit='$id'";
-                }
-                if($index=="сontribution_info"){
-                    $index="сontribution";
-                    $query = "DELETE FROM $database.$index WHERE id_сontribution='$id'";
-                }
-                
+                $query = "DELETE FROM $database.$index WHERE id='$id'";
                 $result = mysqli_query($link, $query) or die ("Ошибка в запросе");
                 header("Location: index.php");
 			}
