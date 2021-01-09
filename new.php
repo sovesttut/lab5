f36a134dc0a930ae9d1b2db8cf4255f7454c3e82
sovesttut <56912470+sovesttut@users.noreply.github.com>
2021-01-09 15:56:35 +0500

    Update new.php

--- new.php
+++ new.php
@@ -6,78 +6,82 @@
             if(isset($_GET['Index'])){
                 $index = htmlentities(mysqli_real_escape_string($link, $_GET['Index']));
                 switch($index){
-                    case "Banks":
-                        echo("<fieldset><legend>Добавить новый банк</legend>");
+                    case "film":
+                        echo("<fieldset><legend>Добавить новый Фильм</legend>");
                         echo("<form id='form' method='post' action='save_new.php'>");
                         
-                        echo("Введите наименование: <input name='bank_name'
-                        type='text' maxlength='60' /> <br>");
+                        echo("Введите Название: <input name='name'
+                        type='text' maxlength='32' /> <br>");
                         
-                        echo("Введите ИНН: <input name='bank_INN'
-                        type='text' maxlength='50' /> <br>");
+                        echo("Введите жанр: <input name='genre'
+                        type='text' maxlength='32' /> <br>");
                         
-                        echo("Введите страну: <input name='bank_strana'
-                        type='text' maxlength='60' /> <br>");
+                        echo("Введите режиссёр: <input name='regisseur'
+                        type='text' maxlength='32' /> <br>");
                         
-                        echo("Введите класс надежности: <input name='bank_class'
-                        type='number' min='1' max='9' value='1'/> <br>");
+                        echo("Введите год выпуска: <input name='year'
+                        type='number' min='1' max='2077' value='2020'/> <br>");
                         
-                        echo("Введите объем активов: <input name='bank_obiem'
-                        type='text' maxlength='40' /> <br>");
+                        echo("Введите кассовые сборы: <input name='cost'
+                        type='number' min='1' max='9999999' value='1'/> <br>");
                         
                         echo("<input type='hidden' name='index' value='$index'> <br>");
                         echo("<input type='submit' value='Добавить'/> <br>");
                         echo("</form>");
                         echo("</fieldset>");
                     break;
-                    case "deposit_programs":
-                        $queryTab = "Banks";
-                        $query = "SELECT * FROM $database.$queryTab ORDER BY $database.$queryTab.id_bank ASC";
-                        $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
-                        echo("<fieldset><legend>Добавить нового программу депозита</legend>");
+                    case "cinema":
+                        echo("<fieldset><legend>Добавить новый Кинозал</legend>");
                         echo("<form id='form' method='post' action='save_new.php'>");
                         
-                        echo("Введите название: <input name='deposit_name'
-                        type='text' maxlength='50'/> <br>");
+                        echo("Введите название зала: <input name='name'
+                        type='text' maxlength='32'/> <br>");
                         
-                        echo("Введите % годовых: <input name='percent'
-                        type='number' min='1' max='9999999' value='1' size='11' /> %<br>");
-                        
-                        $id = "Banks";
-                        echo("<label for='$id'>Список банков: </label>");
-                        echo("<select id='$id' name='$id'>");
-                        echo("<option value=''>--Выберите банк--</option>");
-                        while ($row=mysqli_fetch_array($result)){
-                            echo("<option value='$row[0]'> $row[0]) $row[1]</option>");
-                        }
+                        echo("Введите категорию: <input name='category'
+                        type='text' maxlength='32'/> <br>");
                         
                         echo("<input type='hidden' name='index' value='$index'> <br>");
                         echo("<input type='submit' value='Добавить'/> <br>");
                         echo("</form>");
                         echo("</fieldset>");
                     break;
-                    case "сontribution":
-                        $queryTab = "deposit_programs";
-                        $query = "SELECT * FROM $database.$queryTab ORDER BY $database.$queryTab.id_deposit ASC";
-                        $result = mysqli_query($link, $query) or die("Не могу выполнить запрос!");
-                        echo("<fieldset><legend>Добавить новый вклад</legend>");
+                    case "session":
+                        $queryTab_0 = "film";
+                        $queryTab_1 = "cinema";
+                        $query_0 = "SELECT * FROM $database.$queryTab_0 ORDER BY $database.$queryTab_0.id ASC";
+                        $query_1 = "SELECT * FROM $database.$queryTab_1 ORDER BY $database.$queryTab_1.id ASC";
+                        $result_0 = mysqli_query($link, $query_0) or die("Не могу выполнить запрос!");
+                        $result_1 = mysqli_query($link, $query_1) or die("Не могу выполнить запрос!");
+                        echo("<fieldset><legend>Добавить новый Киносеанс</legend>");
                         echo("<form id='form' method='post' action='save_new.php'>");
                         
-                        echo("Введите дату создания вклада: <input name='date'
-                        type='date'/> <br>");
+                        echo("Введите дату и время начала показа фильма: <input id='date_s' name='date_s'
+                        type='datetime-local'/> <br>");
                         
-                        $id = "deposit_programs";
-                        echo("<label for='$id'>Список программ депозитов: </label>");
-                        echo("<select id='$id' name='$id'>");
-                        echo("<option value=''>--Выберите программу депозита--</option>");
-                        while ($row=mysqli_fetch_array($result)){
+                        $id_0 = "film";
+                        echo("<label for='$id_0'>Список Фильмов: </label>");
+                        echo("<select id='$id_0' name='$id_0'>");
+                        echo("<option value=''>--Please choose an option--</option>");
+                        while ($row=mysqli_fetch_array($result_0)){
                             echo("<option value='$row[0]'> $row[0]) $row[1]</option>");
                         }
                         echo("</select><br>");
-                        echo("Введите  стартовую сумму вклада: <input name='count'
-                        type='number' min='1' max='9999999' value='1' size='11' ><br>");
+                        $id_1 = "cinema";
+                        echo("<label for='$id_1'>Список Кинозалов: </label>");
+                        echo("<select id='$id_1' name='$id_1'>");
+                        echo("<option value=''>--Please choose an option--</option>");
+                        while ($row=mysqli_fetch_array($result_1)){
+                            echo("<option value='$row[0]'> $row[0]) $row[1]</option>");
+                        }
+                        echo("</select><br>");
                         
-                        echo("<input type='hidden' name='index' value='$index'> <br>");
+                        echo("Введите количество мест: <input name='count_f'
+                        type='number' min='1' max='9999999' value='1'/> <br>");
+                        
+                        echo("Введите количество занятых мест: <input name='count_b'
+                        type='number' min='1' max='9999999' value='1'/> <br>");
+                        
+                        echo("<input type='hidden' name='index' value='".$index."'> <br>");
                         echo("<input type='submit' value='Добавить'/> <br>");
                         echo("</form>");
                         echo("</fieldset>");
